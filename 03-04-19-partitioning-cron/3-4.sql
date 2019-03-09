a744f8939a0049107d60bef438d844ef0396ffa1
tolya <flisochcs@gmail.com>
2019-03-09 19:00:41 +0300

    edit insert tg-func: add creating trigger on new partition if partition didn't exist

--- 03-04-19-partitioning-cron/3-4.sql
+++ 03-04-19-partitioning-cron/3-4.sql
@@ -12,10 +12,18 @@ CREATE OR REPLACE FUNCTION insert_partition()
 DECLARE year INTEGER;
 BEGIN
   SELECT EXTRACT(YEAR FROM new.date) INTO year;
-  EXECUTE format(
-      'CREATE TABLE IF NOT EXISTS workout_stats_%s (CONSTRAINT workout_stats_%s_pk PRIMARY KEY(id)) INHERITS(workout_stats)',
-      year, year);
-  RAISE NOTICE 'date: %', new.date;
+  --   creates new partition table and update trigger if Table doesn't exist
+  IF NOT EXISTS(SELECT relname FROM pg_class WHERE relname = 'workout_stats_'||year)
+  THEN
+    EXECUTE format(
+        'CREATE TABLE IF NOT EXISTS workout_stats_%s (CONSTRAINT workout_stats_%s_pk PRIMARY KEY(id)) INHERITS(workout_stats)',
+        year, year);
+    EXECUTE format('CREATE TRIGGER update_data
+      BEFORE UPDATE
+      ON workout_stats_%s
+      FOR EACH ROW EXECUTE PROCEDURE update_partition();', year);
+  END IF;
+  
   EXECUTE format('INSERT INTO workout_stats_%s(date, push_ups, pull_ups, squats) VALUES($1,$2,$3,$4)', year)
   using
     new.date, new.push_ups, new.pull_ups, new.squats;
